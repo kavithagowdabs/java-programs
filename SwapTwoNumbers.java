71c161b07f2bd1014479da99514ebe4820ca911c
kavithagowdabs <42991733+kavithagowdabs@users.noreply.github.com>
2019-05-22 01:03:00 +0530

    Add files via upload

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class SwapTwoNumbers
+{
+ public static void main(String[] args)
+ {
+  LinkedList <String> l_list = new LinkedList <String> ();
+  l_list.add("Red");
+  l_list.add("Green");
+  l_list.add("Black");
+  l_list.add("Pink");
+  l_list.add("orange");
+  System.out.println("The Original linked list: " + l_list);
+  Collections.swap(l_list, 0, 2);
+  System.out.println("The New linked list after swap: " + l_list);
+ }
+}
