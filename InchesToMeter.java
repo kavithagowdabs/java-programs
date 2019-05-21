7c190f43e7b5ca3c52fb861f274830602e7b15bb
kavithagowdabs <42991733+kavithagowdabs@users.noreply.github.com>
2019-05-22 00:03:40 +0530

    Add files via upload

---
+++ InchesToMeter.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class InchesToMeter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+        System.out.print("Input a value for inch: ");
+        double inch = input.nextDouble();
+        double meters = inch * 0.0254;
+        System.out.println(inch + " inch is " + meters + " meters");
+
+	}
+
+}
---
+++ SpecificElemInArray.java
@@ -0,0 +1,24 @@
+
+public class SpecificElemInArray 
+{
+	public static boolean contains(int[] arr, int item) {
+	      for (int n : arr) {
+	         if (item == n) {
+	            return true;
+	         }
+	      }
+	      return false;
+	   }
+	
+	public static void main(String[] args) 
+	{
+		int[] a = {
+	            1789, 2035, 1899, 1456, 2013, 
+	            1458, 2458, 1254, 1472, 2365, 
+	            1456, 2265, 1457, 2456};
+	      System.out.println(contains(a, 2013));
+	      System.out.println(contains(a, 2015));
+
+	}
+
+}
